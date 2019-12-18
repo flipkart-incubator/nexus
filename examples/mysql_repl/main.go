d797b37d33ec4493ee72b9e1e0fbfb47c0ed0ae5
Kalyan Akella <kalyan.akella@gmail.com>
2019-12-18 16:23:26 +0530

    Making mysql_repl a pure Nexus replicator without the periodic inserts

--- examples/mysql_repl/main.go
+++ examples/mysql_repl/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -42,31 +40,6 @@ func init() {
 	stopChan = setupSignalNotify()
 }
 
-var ctr = 0
-
-func save(repl api.RaftReplicator, t time.Time) {
-	key := fmt.Sprintf("%d.Key#%d", nodeId, ctr)
-	val := fmt.Sprintf("%d", t.UnixNano())
-
-	save_req := &store.SaveRequest{
-		StmtTmpl: "insert into sync_table (name, data) values ('{{.name}}', '{{.data}}')",
-		Params: map[string]interface{}{
-			"name": key,
-			"data": val,
-		},
-	}
-	if bts, err := save_req.ToBytes(); err != nil {
-		log.Printf("Error occurred while converting SaveRequest: %s to bytes. Error: %v\n", save_req, err)
-	} else {
-		if err := repl.Replicate(context.Background(), bts); err != nil {
-			log.Printf("Error occurred while replicating SaveRequest: %s. Error: %v\n", save_req, err)
-		} else {
-			log.Printf("Successfully replicated SaveRequest: %s", save_req)
-			ctr++
-		}
-	}
-}
-
 func main() {
 	flag.Parse()
 	if db, err := store.NewMySQLDB(mysqlConnUrl); err != nil {
@@ -80,17 +53,8 @@ func main() {
 			raft.ReplicationTimeout(replTimeout),
 		)
 		repl.Start()
-		ticker := time.NewTicker(5 * time.Second)
-		for {
-			select {
-			case t := <-ticker.C:
-				save(repl, t)
-			case sig := <-stopChan:
-				log.Printf("[WARN] Caught signal: %v. Shutting down...", sig)
-				ticker.Stop()
-				repl.Stop()
-				break
-			}
-		}
+		sig := <-stopChan
+		log.Printf("[WARN] Caught signal: %v. Shutting down...", sig)
+		repl.Stop()
 	}
 }
