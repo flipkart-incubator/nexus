08a9443f4bbb3983563a70d7ac1b7f04910ee141
Kalyan Akella <kalyan.akella@gmail.com>
2020-05-19 17:52:12 +0530

    Support for load operation inside the MySQL and Redis examples

--- examples/mysql_repl/store/db.go
+++ examples/mysql_repl/store/db.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"database/sql"
 	"encoding/gob"
-	"errors"
+	"fmt"
 	"log"
 	"text/template"
 	"time"
@@ -13,24 +13,43 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type LoadRequest struct {
+	StmtTmpl string
+	Params   map[string]interface{}
+}
+
+func (this *LoadRequest) FromBytes(data []byte) error {
+	buf := bytes.NewBuffer(data)
+	if err := gob.NewDecoder(buf).Decode(this); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (this *LoadRequest) ToBytes() ([]byte, error) {
+	return encode(this)
+}
+
 type SaveRequest struct {
 	StmtTmpl string
 	Params   map[string]interface{}
 }
 
-func FromBytes(data []byte) (*SaveRequest, error) {
-	save_req := SaveRequest{}
+func (this *SaveRequest) FromBytes(data []byte) error {
 	buf := bytes.NewBuffer(data)
-	if err := gob.NewDecoder(buf).Decode(&save_req); err != nil {
-		return nil, err
-	} else {
-		return &save_req, nil
+	if err := gob.NewDecoder(buf).Decode(this); err != nil {
+		return err
 	}
+	return nil
 }
 
 func (this *SaveRequest) ToBytes() ([]byte, error) {
+	return encode(this)
+}
+
+func encode(obj interface{}) ([]byte, error) {
 	var buf bytes.Buffer
-	if err := gob.NewEncoder(&buf).Encode(this); err != nil {
+	if err := gob.NewEncoder(&buf).Encode(obj); err != nil {
 		return nil, err
 	}
 	return buf.Bytes(), nil
@@ -58,31 +77,83 @@ func (this *mysqlStore) Close() error {
 
 const txTimeout = 5 * time.Second // TODO: Should be configurable
 
-func (this *mysqlStore) save(sqlStmt string) error {
+func (this *mysqlStore) load(sqlStmt string) (*sql.Rows, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), txTimeout)
+	defer cancel()
+
+	// TODO: Isolation level can be part of the SaveRequest itself ?
+	if tx, err := this.db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable, ReadOnly: true}); err != nil {
+		return nil, err
+	} else {
+		log.Printf("About to execute SQL: %s", sqlStmt)
+		return tx.QueryContext(ctx, sqlStmt)
+	}
+}
+
+func (this *mysqlStore) save(sqlStmt string) (sql.Result, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), txTimeout)
 	defer cancel()
 
 	// TODO: Isolation level can be part of the SaveRequest itself ?
 	if tx, err := this.db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable}); err != nil {
-		return err
+		return nil, err
 	} else {
 		log.Printf("About to execute SQL: %s", sqlStmt)
-		if _, err := tx.ExecContext(ctx, sqlStmt); err != nil {
-			tx.Rollback() // TODO: Ignore rollback errors ?
-			return err
+		if res, err := tx.ExecContext(ctx, sqlStmt); err != nil {
+			tx.Rollback()
+			return nil, err
 		} else {
 			log.Printf("Successfully executed SQL: %s", sqlStmt)
-			return tx.Commit()
+			return res, tx.Commit()
 		}
 	}
 }
 
 func (this *mysqlStore) Load(data []byte) ([]byte, error) {
-	return nil, errors.New("Not implemented yet")
+	var load_req LoadRequest
+	if err := load_req.FromBytes(data); err != nil {
+		return nil, err
+	} else {
+		sql := load_req.StmtTmpl
+		if tmpl, err := template.New("sql_tmpl").Parse(sql); err != nil {
+			return nil, err
+		} else {
+			var buf bytes.Buffer
+			if err := tmpl.Execute(&buf, load_req.Params); err != nil {
+				return nil, err
+			} else {
+				if rows, err := this.load(buf.String()); err != nil {
+					return nil, err
+				} else {
+					defer rows.Close()
+					var results []map[string]interface{}
+					cols, _ := rows.Columns()
+					for rows.Next() {
+						columns := make([]interface{}, len(cols))
+						columnPointers := make([]interface{}, len(cols))
+						for i, _ := range columns {
+							columnPointers[i] = &columns[i]
+						}
+						if err := rows.Scan(columnPointers...); err != nil {
+							return nil, err
+						}
+						row := make(map[string]interface{})
+						for i, colName := range cols {
+							val := columnPointers[i].(*interface{})
+							row[colName] = *val
+						}
+						results = append(results, row)
+					}
+					return []byte(fmt.Sprintf("%v", results)), rows.Err()
+				}
+			}
+		}
+	}
 }
 
 func (this *mysqlStore) Save(data []byte) ([]byte, error) {
-	if save_req, err := FromBytes(data); err != nil {
+	var save_req SaveRequest
+	if err := save_req.FromBytes(data); err != nil {
 		return nil, err
 	} else {
 		sql := save_req.StmtTmpl
@@ -93,7 +164,11 @@ func (this *mysqlStore) Save(data []byte) ([]byte, error) {
 			if err := tmpl.Execute(&buf, save_req.Params); err != nil {
 				return nil, err
 			} else {
-				return nil, this.save(buf.String())
+				if res, err := this.save(buf.String()); err != nil {
+					return nil, err
+				} else {
+					return []byte(fmt.Sprintf("%#v", res)), nil
+				}
 			}
 		}
 	}
--- examples/repl/main.go
+++ examples/repl/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	str "strings"
@@ -11,7 +11,7 @@ import (
 )
 
 func printUsage() {
-	fmt.Printf("Usage: %s <mysql|redis> <nexus_url> [<expression>]\n", os.Args[0])
+	fmt.Printf("Usage: %s <mysql|redis> <nexus_url> load|save <expression>\n", os.Args[0])
 }
 
 func newNexusClient(nexus_url string) *grpc.NexusClient {
@@ -22,96 +22,81 @@ func newNexusClient(nexus_url string) *grpc.NexusClient {
 	}
 }
 
-func sendMySQLCmd(nc *grpc.NexusClient, cmd string) ([]byte, error) {
+func loadMySQLCmd(nc *grpc.NexusClient, cmd string) ([]byte, error) {
+	load_req := &mstore.LoadRequest{StmtTmpl: cmd}
+	if bts, err := load_req.ToBytes(); err != nil {
+		return nil, err
+	} else {
+		return nc.Load(bts)
+	}
+}
+
+func saveMySQLCmd(nc *grpc.NexusClient, cmd string) ([]byte, error) {
 	save_req := &mstore.SaveRequest{StmtTmpl: cmd}
 	if bts, err := save_req.ToBytes(); err != nil {
 		return nil, err
 	} else {
-		return nc.Replicate(bts)
+		return nc.Save(bts)
 	}
 }
 
-func sendRedisCmd(nc *grpc.NexusClient, cmd string) ([]byte, error) {
-	return nc.Replicate([]byte(cmd))
-}
-
-func sendMySQL(nexus_url string, cmd string) {
-	nc := newNexusClient(nexus_url)
-	defer nc.Close()
-
-	if res, err := sendMySQLCmd(nc, str.TrimSpace(cmd)); err != nil {
-		fmt.Println(err.Error())
-	} else {
-		fmt.Printf("Response from MySQL (without quotes): '%s'\n", res)
+func sendRedisCmd(nc *grpc.NexusClient, mode, cmd string) ([]byte, error) {
+	switch str.ToLower(mode) {
+	case "load":
+		return nc.Load([]byte(cmd))
+	case "save":
+		return nc.Save([]byte(cmd))
 	}
+	return nil, errors.New("Unknown mode: " + mode)
 }
 
-func replMySQL(nexus_url string) {
+func sendMySQL(nexus_url string, mode, cmd string) {
 	nc := newNexusClient(nexus_url)
 	defer nc.Close()
 
-	in := bufio.NewScanner(os.Stdin)
-	fmt.Print("mysql> ")
-	for in.Scan() {
-		cmd := in.Text()
-		if res, err := sendMySQLCmd(nc, str.TrimSpace(cmd)); err != nil {
+	switch str.ToLower(str.TrimSpace(mode)) {
+	case "load":
+		if res, err := loadMySQLCmd(nc, str.TrimSpace(cmd)); err != nil {
 			fmt.Println(err.Error())
 		} else {
 			fmt.Printf("Response from MySQL (without quotes): '%s'\n", res)
 		}
-		fmt.Print("mysql> ")
+	case "save":
+		if res, err := saveMySQLCmd(nc, str.TrimSpace(cmd)); err != nil {
+			fmt.Println(err.Error())
+		} else {
+			fmt.Printf("Response from MySQL (without quotes): '%s'\n", res)
+		}
+	default:
+		fmt.Printf("Unknown mode: " + mode)
 	}
+
 }
 
-func sendRedis(nexus_url string, cmd string) {
+func sendRedis(nexus_url string, mode, cmd string) {
 	nc := newNexusClient(nexus_url)
 	defer nc.Close()
 
-	if res, err := sendRedisCmd(nc, str.TrimSpace(cmd)); err != nil {
+	if res, err := sendRedisCmd(nc, str.TrimSpace(mode), str.TrimSpace(cmd)); err != nil {
 		fmt.Println(err.Error())
 	} else {
 		fmt.Printf("Response from Redis (without quotes): '%s'\n", res)
 	}
 }
 
-func replRedis(nexus_url string) {
-	nc := newNexusClient(nexus_url)
-	defer nc.Close()
-
-	in := bufio.NewScanner(os.Stdin)
-	fmt.Print("redis> ")
-	for in.Scan() {
-		cmd := in.Text()
-		if res, err := sendRedisCmd(nc, str.TrimSpace(cmd)); err != nil {
-			fmt.Println(err.Error())
-		} else {
-			fmt.Printf("Response from Redis (without quotes): '%s'\n", res)
-		}
-		fmt.Print("redis> ")
-	}
-}
-
 func main() {
 	arg_len := len(os.Args)
-	if arg_len < 3 || arg_len > 4 {
+	if arg_len != 5 {
 		printUsage()
 		return
 	}
 
-	db, nexus_url, repl_mode := str.ToLower(str.TrimSpace(os.Args[1])), str.TrimSpace(os.Args[2]), arg_len == 3
+	db, nexus_url := str.ToLower(str.TrimSpace(os.Args[1])), str.TrimSpace(os.Args[2])
 	switch db {
 	case "mysql":
-		if repl_mode {
-			replMySQL(nexus_url)
-		} else {
-			sendMySQL(nexus_url, os.Args[3])
-		}
+		sendMySQL(nexus_url, os.Args[3], os.Args[4])
 	case "redis":
-		if repl_mode {
-			replRedis(nexus_url)
-		} else {
-			sendRedis(nexus_url, os.Args[3])
-		}
+		sendRedis(nexus_url, os.Args[3], os.Args[4])
 	default:
 		printUsage()
 	}
